e1a203510a4a7ceddfa4e9ef8c1f39c0e82ade22
fahadbgnr <aminulislamfahad1@gmail.com>
2025-03-01 18:05:06 +0600

    complete section done

---
+++ script/btn-container.js
@@ -0,0 +1,134 @@
+document.getElementsByClassName('btn-primary')[0].addEventListener('click', function(event){
+    event.preventDefault();
+    const plusItems = document.getElementById('plus-items').value;
+    const convertedPlusItems = parseInt(plusItems);
+    const oddItems = document.getElementById('odd-items').value;
+    const convertedOddItems = parseInt(oddItems);
+   if(event.target){
+    alert('Fix Mobile Button Issue')
+    const sum = 23 + 1 ;
+    document.getElementById('plus-items').innerText=sum;
+
+    const Sum = 6 - 1;
+    document.getElementById('odd-items').innerText = Sum;
+    
+   }
+   else{
+    alert('thik nai')
+   }
+
+   
+
+})
+
+
+document.getElementsByClassName('btn-primary')[1].addEventListener('click', function(event){
+    event.preventDefault();
+    const plusItems = document.getElementById('plus-items').value;
+    const convertedPlusItems = parseInt(plusItems);
+    const oddItems = document.getElementById('odd-items').value;
+    const convertedOddItems = parseInt(oddItems);
+   if(event.target){
+    alert('Add Dark Mode')
+    const sum = 24 + 1 ;
+    document.getElementById('plus-items').innerText=sum;
+
+    const Sum = 5 - 1;
+    document.getElementById('odd-items').innerText = Sum;
+    
+   }
+   else{
+    alert('thik nai')
+   }
+
+   
+
+})
+document.getElementsByClassName('btn-primary')[2].addEventListener('click', function(event){
+    event.preventDefault();
+    const plusItems = document.getElementById('plus-items').value;
+    const convertedPlusItems = parseInt(plusItems);
+    const oddItems = document.getElementById('odd-items').value;
+    const convertedOddItems = parseInt(oddItems);
+   if(event.target){
+    alert('Optimize Home page ')
+    const sum = 25 + 1 ;
+    document.getElementById('plus-items').innerText=sum;
+
+    const Sum = 4 - 1;
+    document.getElementById('odd-items').innerText = Sum;
+    
+   }
+   else{
+    alert('thik nai')
+   }
+
+   
+
+})
+document.getElementsByClassName('btn-primary')[3].addEventListener('click', function(event){
+    event.preventDefault();
+    const plusItems = document.getElementById('plus-items').value;
+    const convertedPlusItems = parseInt(plusItems);
+    const oddItems = document.getElementById('odd-items').value;
+    const convertedOddItems = parseInt(oddItems);
+   if(event.target){
+    alert('Add new emoji 🤲')
+    const sum = 26 + 1 ;
+    document.getElementById('plus-items').innerText=sum;
+
+    const Sum = 3 - 1;
+    document.getElementById('odd-items').innerText = Sum;
+    
+   }
+   else{
+    alert('thik nai')
+   }
+
+   
+
+})
+document.getElementsByClassName('btn-primary')[4].addEventListener('click', function(event){
+    event.preventDefault();
+    const plusItems = document.getElementById('plus-items').value;
+    const convertedPlusItems = parseInt(plusItems);
+    const oddItems = document.getElementById('odd-items').value;
+    const convertedOddItems = parseInt(oddItems);
+   if(event.target){
+    alert('Integrate OpenAI API')
+    const sum = 27 + 1 ;
+    document.getElementById('plus-items').innerText=sum;
+
+    const Sum = 2 - 1;
+    document.getElementById('odd-items').innerText = Sum;
+    
+   }
+   else{
+    alert('thik nai')
+   }
+
+   
+
+})
+document.getElementsByClassName('btn-primary')[5].addEventListener('click', function(event){
+    event.preventDefault();
+    const plusItems = document.getElementById('plus-items').value;
+    const convertedPlusItems = parseInt(plusItems);
+    const oddItems = document.getElementById('odd-items').value;
+    const convertedOddItems = parseInt(oddItems);
+   if(event.target){
+    alert('Improve Job searching')
+    const sum = 29 + 1 ;
+    document.getElementById('plus-items').innerText=sum;
+
+    const Sum = 1 - 1;
+    document.getElementById('odd-items').innerText = Sum;
+    
+   }
+   else{
+    alert('thik nai')
+   }
+
+   
+
+})
---
+++ script/update-date.js
@@ -0,0 +1,11 @@
+document.getElementById('update-date')
+.addEventListener('')
+
+const date = new date();
+
+ let day = date.getdate();
+ let mouth = date.getmonth() + 1;
+ let year =date.getfullyear();
+
+ let currentDate =`$ {day}-${month}-${year}`
+ console.log(currentDate);
