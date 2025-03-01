c8941d0b1409e701e54fda5afd52077d6a9e6540
fahadbgnr <aminulislamfahad1@gmail.com>
2025-03-02 01:44:18 +0600

    project allmost done

--- script/btn-container.js
+++ script/btn-container.js
@@ -4,6 +4,8 @@ document.getElementsByClassName('btn-primary')[0].addEventListener('click', func
     const convertedPlusItems = parseInt(plusItems);
     const oddItems = document.getElementById('odd-items').value;
     const convertedOddItems = parseInt(oddItems);
+    const btnText = document.getElementById('btn-1-text').innerText;
+    const now = new Date();
    if(event.target){
     alert('Fix Mobile Button Issue')
     const sum = 23 + 1 ;
@@ -13,6 +15,14 @@ document.getElementsByClassName('btn-primary')[0].addEventListener('click', func
     document.getElementById('odd-items').innerText = Sum;
     const buttons = document.querySelectorAll(".btn-1");
     buttons.forEach(button => button.disabled = true);
+
+    const container =document.getElementById('activity-container');
+
+    const p = document.createElement('p');
+    p.innerText = `
+     You have completed the task ${btnText} at ${now.toLocaleTimeString()}
+    `
+    container.appendChild(p);
 
     
     
@@ -34,6 +44,8 @@ document.getElementsByClassName('btn-primary')[1].addEventListener('click', func
     const convertedPlusItems = parseInt(plusItems);
     const oddItems = document.getElementById('odd-items').value;
     const convertedOddItems = parseInt(oddItems);
+    const btnText = document.getElementById('btn-2-text').innerText;
+    const now = new Date();
    if(event.target){
     alert('Add Dark Mode')
     const sum = 24 + 1 ;
@@ -43,6 +55,14 @@ document.getElementsByClassName('btn-primary')[1].addEventListener('click', func
     document.getElementById('odd-items').innerText = Sum;
     const buttons = document.querySelectorAll(".btn-2");
     buttons.forEach(button => button.disabled = true);
+
+    const container =document.getElementById('activity-container');
+
+    const p = document.createElement('p');
+    p.innerText = `
+     You have completed the task ${btnText} at ${now.toLocaleTimeString()}
+    `
+    container.appendChild(p);
     
    }
    else{
@@ -58,6 +78,8 @@ document.getElementsByClassName('btn-primary')[2].addEventListener('click', func
     const convertedPlusItems = parseInt(plusItems);
     const oddItems = document.getElementById('odd-items').value;
     const convertedOddItems = parseInt(oddItems);
+    const btnText = document.getElementById('btn-3-text').innerText;
+    const now = new Date();
    if(event.target){
     alert('Optimize Home page ')
     const sum = 25 + 1 ;
@@ -67,6 +89,14 @@ document.getElementsByClassName('btn-primary')[2].addEventListener('click', func
     document.getElementById('odd-items').innerText = Sum;
     const buttons = document.querySelectorAll(".btn-3");
     buttons.forEach(button => button.disabled = true);
+
+    const container =document.getElementById('activity-container');
+
+    const p = document.createElement('p');
+    p.innerText = `
+     You have completed the task ${btnText} at ${now.toLocaleTimeString()}
+    `
+    container.appendChild(p);
     
    }
    else{
@@ -82,6 +112,8 @@ document.getElementsByClassName('btn-primary')[3].addEventListener('click', func
     const convertedPlusItems = parseInt(plusItems);
     const oddItems = document.getElementById('odd-items').value;
     const convertedOddItems = parseInt(oddItems);
+    const btnText = document.getElementById('btn-4-text').innerText;
+    const now = new Date();
    if(event.target){
     alert('Add new emoji 🤲')
     const sum = 26 + 1 ;
@@ -91,6 +123,14 @@ document.getElementsByClassName('btn-primary')[3].addEventListener('click', func
     document.getElementById('odd-items').innerText = Sum;
     const buttons = document.querySelectorAll(".btn-4");
     buttons.forEach(button => button.disabled = true);
+
+    const container =document.getElementById('activity-container');
+
+    const p = document.createElement('p');
+    p.innerText = `
+     You have completed the task ${btnText} at ${now.toLocaleTimeString()}
+    `
+    container.appendChild(p);
     
    }
    else{
@@ -106,6 +146,8 @@ document.getElementsByClassName('btn-primary')[4].addEventListener('click', func
     const convertedPlusItems = parseInt(plusItems);
     const oddItems = document.getElementById('odd-items').value;
     const convertedOddItems = parseInt(oddItems);
+    const btnText = document.getElementById('btn-5-text').innerText;
+    const now = new Date();
    if(event.target){
     alert('Integrate OpenAI API')
     const sum = 27 + 1 ;
@@ -115,6 +157,14 @@ document.getElementsByClassName('btn-primary')[4].addEventListener('click', func
     document.getElementById('odd-items').innerText = Sum;
     const buttons = document.querySelectorAll(".btn-5");
     buttons.forEach(button => button.disabled = true);
+
+    const container =document.getElementById('activity-container');
+
+    const p = document.createElement('p');
+    p.innerText = `
+     You have completed the task ${btnText} at ${now.toLocaleTimeString()}
+    `
+    container.appendChild(p);
     
    }
    else{
@@ -130,6 +180,8 @@ document.getElementsByClassName('btn-primary')[5].addEventListener('click', func
     const convertedPlusItems = parseInt(plusItems);
     const oddItems = document.getElementById('odd-items').value;
     const convertedOddItems = parseInt(oddItems);
+    const btnText = document.getElementById('btn-6-text').innerText;
+    const now = new Date();
    if(event.target){
     alert('Improve Job searching')
     alert('Congrates!!! you have complete all the current task')
@@ -140,6 +192,14 @@ document.getElementsByClassName('btn-primary')[5].addEventListener('click', func
     document.getElementById('odd-items').innerText = Sum;
     const buttons = document.querySelectorAll(".btn-6");
     buttons.forEach(button => button.disabled = true);
+
+    const container =document.getElementById('activity-container');
+
+    const p = document.createElement('p');
+    p.innerText = `
+     You have completed the task ${btnText} at ${now.toLocaleTimeString()}
+    `
+    container.appendChild(p);
     
    }
    else{
