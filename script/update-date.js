3385020f071803d34790079a028c322da4a1d402
fahadbgnr <aminulislamfahad1@gmail.com>
2025-03-02 01:52:48 +0600

    project done

--- script/update-date.js
+++ script/update-date.js
@@ -1,11 +1,8 @@
-document.getElementById('update-date')
-.addEventListener('')
+function updateDateTime() {
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    document.getElementById("datetime").innerText = now.toLocaleDateString('en-US', options);
+}
 
-const date = new date();
-
- let day = date.getdate();
- let mouth = date.getmonth() + 1;
- let year =date.getfullyear();
-
- let currentDate =`$ {day}-${month}-${year}`
- console.log(currentDate);
+setInterval(updateDateTime, 1000);
+updateDateTime();
